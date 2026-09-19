384d25de59810e71e0b0c4df93cfabe12d41cc0a
Muhammad Faseeh Ur Rehman <muhammadfaseehurrehman05@gmail.com>
2026-09-19 13:10:28 +0500

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import { CartProvider } from './context/CartContext.jsx'
 import { ToastContainer } from 'react-toastify'
 import ScrollToTop from 'react-scroll-to-top'
 
-// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -16,7 +15,6 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
   <DataProvider>
     <CartProvider>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
@@ -37,5 +35,4 @@ createRoot(document.getElementById('root')).render(
       </ClerkProvider>
     </CartProvider>
   </DataProvider>
-  // </StrictMode>,
 )
